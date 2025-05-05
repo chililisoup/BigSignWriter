4f6e76f9b2fd86696c0a5450818b01e321ff331e
DebornMC <66801882+DebornMC@users.noreply.github.com>
2025-05-06 01:46:41 +0200

    Add files via upload

---
+++ src/main/java/dev/chililisoup/bigsignwriter/FontFile.java
@@ -0,0 +1,9 @@
+package dev.chililisoup.bigsignwriter;
+
+import java.util.Map;
+
+public class FontFile {
+    public String name;
+    public String characterSeparator;
+    public Map<Character, String[]> characters;
+}
